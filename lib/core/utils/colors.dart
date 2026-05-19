f528f5baf74c4efb742bf81fbfcd5b982787de0b
david <davidyluni.personal@gmail.com>
2026-05-19 16:57:59 +0200

    ✨ Add new color utility

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -1,7 +1,9 @@
-
 import 'package:flutter/material.dart';
 
-
-String colorToHexString (Color color) {
+String colorToHex(Color color) {
   return color.toString().replaceAll('Color(0x', '#').replaceAll(')', '');
+}
+
+Color hexStringToColor(String hex) {
+  return Color(int.parse(hex.replaceAll('#', '0xFF')));
 }
