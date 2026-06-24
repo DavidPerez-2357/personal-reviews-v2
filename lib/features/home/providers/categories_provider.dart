a7e23faa1e5fd75d46bf4841a5317ad4b0943615
david <davidyluni.personal@gmail.com>
2026-06-24 14:50:58 +0200

    🐛 Change watch with get

--- lib/features/home/providers/categories_provider.dart
+++ lib/features/home/providers/categories_provider.dart
@@ -2,9 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:personal_reviews/providers/repositories_provider.dart';
 import 'package:personal_reviews/domain/models/category.dart';
 
-final categoriesProvider = StreamProvider.autoDispose<List<CategoryWithStats>>((
+final categoriesProvider = FutureProvider.autoDispose<List<CategoryWithStats>>((
   ref,
 ) {
   final categoryRepository = ref.watch(categoryRepositoryProvider);
-  return categoryRepository.watchAllWithStats(false);
+  return categoryRepository.getAllWithStats(false);
 });
