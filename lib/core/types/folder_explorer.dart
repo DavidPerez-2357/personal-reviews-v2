d65f9f7edaba2470d6e2347645a0a9ddba396696
david <davidyluni.personal@gmail.com>
2026-06-24 14:38:56 +0200

    ✨ Convert folder explorer types to frezzed and add explorer params

--- lib/core/types/folder_explorer.dart
+++ lib/core/types/folder_explorer.dart
@@ -1,35 +1,46 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:personal_reviews/core/types/elements_filter.dart';
 import 'package:personal_reviews/core/types/elements_sort.dart';
+import 'package:personal_reviews/domain/models/folder.dart';
+import 'package:personal_reviews/domain/models/item.dart';
+part 'folder_explorer.freezed.dart';
 
-class FolderExplorerConfig {
-  /* DB Options */
-  final bool groupByFolders;
-  final int? folderId;
-  final bool includeDeleted;
-  final bool excludeNonDeleted;
+@freezed
+abstract class FolderExplorerConfig with _$FolderExplorerConfig {
+  const factory FolderExplorerConfig({
+    /* DB Options */
+    @Default(true) bool groupByFolders,
+    int? folderId,
+    @Default(false) bool includeDeleted,
+    @Default(false) bool excludeNonDeleted,
 
-  /* UI options */
-  final bool showSearch;
-  final bool showSort;
-  final bool showFilter;
-  final bool showCategoriesFilter;
-  final bool showVisibilityFilter;
+    /* UI options */
+    @Default(true) bool showSearch,
+    @Default(true) bool showSort,
+    @Default(true) bool showFilter,
+    @Default(true) bool showCategoriesFilter,
+    @Default(true) bool showVisibilityFilter,
 
-  /* Default filter and sort */
-  final ElementsFilter defaultFilter;
-  final ElementsSort defaultSort;
+    /* Default filter and sort */
+    @Default(ElementsFilter()) ElementsFilter defaultFilter,
+    @Default(ElementsSort()) ElementsSort defaultSort,
+  }) = _FolderExplorerConfig;
+}
 
-  const FolderExplorerConfig({
-    this.groupByFolders = true,
-    this.folderId,
-    this.includeDeleted = false,
-    this.excludeNonDeleted = false,
-    this.showSearch = true,
-    this.showSort = true,
-    this.showFilter = true,
-    this.showCategoriesFilter = true,
-    this.showVisibilityFilter = true,
-    this.defaultFilter = const ElementsFilter(),
-    this.defaultSort = const ElementsSort(),
-  });
+@freezed
+abstract class FolderExplorerData with _$FolderExplorerData {
+  const factory FolderExplorerData({
+    @Default(false) bool hasFolders,
+    @Default(false) bool hasItems,
+    @Default([]) List<FolderDetailedNode> folders,
+    @Default([]) List<ItemWithLastReview> items,
+  }) = _FolderExplorerData;
+}
+
+@freezed
+abstract class FolderExplorerParams with _$FolderExplorerParams {
+  const factory FolderExplorerParams({
+    required FolderExplorerConfig config,
+    required FolderExplorerData data,
+  }) = _FolderExplorerParams;
 }
