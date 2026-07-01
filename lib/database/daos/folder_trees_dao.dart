9936e06d599e533d02546e5092cc46c61711f5b9
david <davidyluni.personal@gmail.com>
2026-07-01 15:40:08 +0200

    🗃️ Create folder trees table and dao

---
+++ lib/database/daos/folder_trees_dao.dart
@@ -0,0 +1,93 @@
+import 'package:drift/drift.dart';
+import 'package:personal_reviews/database/app_database.dart';
+import 'package:personal_reviews/database/tables/folder_trees_table.dart';
+part 'folder_trees_dao.g.dart';
+
+@DriftAccessor(tables: [FolderTrees])
+class FolderTreesDao extends DatabaseAccessor<AppDatabase>
+    with _$FolderTreesDaoMixin {
+  FolderTreesDao(super.attachedDatabase);
+
+  /// Get all successors of a folder, including the folder itself.
+  Future<List<FolderTree>> getByDescendantId(int descendantId) {
+    return (select(
+      folderTrees,
+    )..where((tbl) => tbl.descendantId.equals(descendantId))).get();
+  }
+
+  /// Get all predecessors of a folder, including the folder itself.
+  Future<List<FolderTree>> getByAncestorId(int ancestorId) {
+    return (select(
+      folderTrees,
+    )..where((tbl) => tbl.ancestorId.equals(ancestorId))).get();
+  }
+
+  Future<List<int>> getIdsByAncestorId(int ancestorId) async {
+    final results = await getByAncestorId(ancestorId);
+    return results.map((row) => row.descendantId).toList();
+  }
+
+  Future<List<int>> getIdsByAncestorIds(List<int> ancestorIds) async {
+    return (select(folderTrees)
+          ..where((tbl) => tbl.ancestorId.isIn(ancestorIds)))
+        .get()
+        .then((rows) => rows.map((row) => row.descendantId).toList());
+  }
+
+  Future<List<int>> getIdsByDescendantId(int descendantId) async {
+    final results = await getByDescendantId(descendantId);
+    return results.map((row) => row.ancestorId).toList();
+  }
+
+  /// Inserts all relations in the folder tree for a new folder.
+  Future<void> insertFolderTree(int folderId, int? parentId) {
+    return batch((batch) async {
+      // Insert the relation to itself
+      batch.insert(
+        folderTrees,
+        FolderTree(descendantId: folderId, ancestorId: folderId, depth: 0),
+      );
+
+      if (parentId == null) {
+        return;
+      }
+
+      // Insert relations to ancestors
+      final ancestors = await getByDescendantId(parentId);
+
+      for (final ancestor in ancestors) {
+        batch.insert(
+          folderTrees,
+          FolderTree(
+            descendantId: folderId,
+            ancestorId: ancestor.ancestorId,
+            depth: ancestor.depth + 1,
+          ),
+        );
+      }
+    });
+  }
+
+  /// This is useful when deleting a folder, its subfolders and relations in the folder tree.
+  Future<void> deleteFolderTree(int folderTreeId) {
+    return (delete(folderTrees)..where(
+          (tbl) =>
+              tbl.descendantId.equals(folderTreeId) |
+              tbl.ancestorId.equals(folderTreeId),
+        ))
+        .go();
+  }
+
+  /// This only deletes the folder if not used by any other folder as a parent.
+  Future<bool> safeDeleteFolderTree(int folderTreeId) async {
+    final isUsedAsParent = await getByAncestorId(folderTreeId);
+
+    if (isUsedAsParent.length > 1) {
+      return false;
+    }
+
+    await deleteFolderTree(folderTreeId);
+
+    return true;
+  }
+}
---
+++ lib/database/tables/folder_trees_table.dart
@@ -0,0 +1,13 @@
+import 'package:drift/drift.dart';
+import 'package:personal_reviews/database/tables/folders_table.dart';
+
+@TableIndex(name: 'folder_trees_index', columns: {#descendantId, #ancestorId})
+class FolderTrees extends Table {
+  IntColumn get descendantId =>
+      integer().references(Folders, #id, onDelete: KeyAction.cascade)();
+
+  IntColumn get ancestorId =>
+      integer().references(Folders, #id, onDelete: KeyAction.cascade)();
+
+  IntColumn get depth => integer().withDefault(const Constant(0))();
+}
